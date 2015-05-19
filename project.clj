2d5216c0d4979351e19a481e935ca184bdfb1ec4
kovasb <kovas.boguta@gmail.com>
2015-05-18 22:33:40 -0400

    tagged versions

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject gamma-examples "0.1.0-SNAPSHOT"
+(defproject gamma-examples "auto"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3291"]
-                 [kovasb/gamma "0.1.2-SNAPSHOT"]
-                 [kovasb/gamma-driver "0.1.0-SNAPSHOT"]
-                 [thi.ng/geom "0.0-715"]])
+                 [kovasb/gamma "0.0-135"]
+                 [kovasb/gamma-driver "0.0-26"]
+                 [thi.ng/geom "0.0-715"]]
+  :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.0"]])
