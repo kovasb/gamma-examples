426bd2712db67578ca4bcd6f4d645e234b7d0bc7
kovasb <kovas.boguta@gmail.com>
2015-05-23 16:45:06 -0400

    bump deps

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
                  [org.clojure/clojurescript "0.0-3292"]
                  [kovasb/gamma "0.0-135"]
-                 [kovasb/gamma-driver "0.0-34-14-gdd56**DIRTY**"]
+                 [kovasb/gamma-driver "0.0-49"]
                  [thi.ng/geom "0.0-715"]]
   :plugins [[org.clojars.cvillecsteele/lein-git-version "1.0.0"]])
